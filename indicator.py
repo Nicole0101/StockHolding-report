f5124a423cadfa2e047d238a59a289296e3898f6
Nicole0101 <33416598+Nicole0101@users.noreply.github.com>
2026-03-28 20:30:43 +0800

    Update indicator.py

--- indicator.py
+++ indicator.py
@@ -1,1 +1,10 @@
-Print(indicator.py)
+import pandas_ta as ta
+
+def add_indicators(df):
+    df['K'], df['D'] = ta.stoch(df['close'], df['close'], df['close'])
+    bb = ta.bbands(df['close'])
+
+    df['bb_upper'] = bb['BBU_5_2.0']
+    df['bb_lower'] = bb['BBL_5_2.0']
+
+    return df
