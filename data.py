d16d4c60e3e365be43f6389024a4e114c883a151
Nicole0101 <33416598+Nicole0101@users.noreply.github.com>
2026-03-28 20:30:21 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,9 @@
-print("data file")
+import requests
+import pandas as pd
+
+def get_stock_data(stock_id):
+    url = f"https://query1.finance.yahoo.com/v7/finance/chart/{stock_id}.TW"
+    res = requests.get(url).json()
+
+    close = res['chart']['result'][0]['indicators']['quote'][0]['close']
+    return pd.DataFrame({"close": close})
